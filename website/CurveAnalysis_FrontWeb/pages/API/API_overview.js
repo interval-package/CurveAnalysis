5b91c76ba9f41677f52783ab5a5bed72725c903b
Zza <1519162203@qq.com>
2022-10-07 20:21:58 +0800

    add app

---
+++ website/CurveAnalysis_FrontWeb/pages/API/API_overview.js
@@ -0,0 +1,17 @@
+
+function open_doc(evt, info) {
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+tablinks = document.getElementsByClassName("tablinks");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+}
+document.getElementById(info).style.display = "block";
+evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
---
+++ website/CurveAnalysis_Server/CurveAnalysis/api_view/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiViewConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api_view"
